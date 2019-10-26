b11366f64025cd83fc28d2f2d2d9d8befb56cdd5
dijahp <dijah@bellsouth.net>
2019-10-26 12:18:21 -0400

    Revamped dashboard and creted extra space for more components

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom";
 import "../assets/Card.css";
 
 class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { iconImg } = this.props;
     return (
       <div className='Card'>
-        <div className='Card-card'>
-          <i>H</i>
-        </div>
         <div className='Card-items'>
-          <p>
-            Example of where current temp/humidity whatever can be placed and
-            passed with props
-          </p>
+          <div className='Card-img'>
+            <img src={iconImg}></img>
+          </div>
+          <div className='Card-info'>
+            <p className='Card-title'>Current Temperature</p>
+            <p className='Card-data'>30&deg;</p>
+          </div>
         </div>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,15 @@ import ReactDOM from "react-dom";
 import "../assets/Header.css";
 
 class Header extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       name: "Test_Name"
-    }
+    };
   }
   render() {
     return (
       <div className='Header'>
-        <h2>Dashboard</h2>
         <h4>Welcome, {this.state.name}</h4>
       </div>
     );
