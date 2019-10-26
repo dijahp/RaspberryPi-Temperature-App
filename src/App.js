dbcbd4535da84fee938039e01dd23b8c0a05c046
Sherri Booher <sherribooher74@gmail.com>
2019-10-26 18:59:35 -0400

    Add routes to App.js for default SignIn page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import SignOut from "./components/SignOut";
 
-function App() {
-  return (
-    <div className='App'>
-      <Dashboard />
+import * as ROUTES from '../src/constants/routes';
+
+const App = () => (
+  <Router>
+    <div>
+      <SignIn />
+
+      <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+      <Route path={ROUTES.SIGN_UP} component={SignUp} />
+      <Route path={ROUTES.SIGN_OUT} component={SignOut} />
+      <Route path={ROUTES.DASHBOARD} component={Dashboard} />
     </div>
-  );
-}
+  </Router>
+);
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Dashboard />
+//     </div>
+//   );
+// }
 
 export default App;
