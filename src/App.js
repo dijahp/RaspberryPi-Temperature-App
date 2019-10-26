df514a7d7c41a97864c674e1a99d97a4b449e593
Sherri Booher <sherribooher74@gmail.com>
2019-10-26 18:19:29 -0400

    Readd import statement for "./App.css"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import "./App.css";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import SignOut from "./components/SignOut";
