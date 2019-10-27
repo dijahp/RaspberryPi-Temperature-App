edbe10095f03944b5c6e3d77f16421ceed1fc0b2
dijahp <dijah@bellsouth.net>
2019-10-26 21:39:18 -0400

    commiting before merge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import SignIn from "./components/SignIn/SignIn";
-import SignUp from "./components/SignUp/SignUp";
-import SignOut from "./components/SignOut/SignOut";
+import SignIn from "./components/SignIn/Signin";
+import SignUp from "./components/SignUp/Signup";
+import SignOut from "./components/SignOut/Signout";
 
 import * as ROUTES from "./constants/routes";
 
 const App = () => (
   <Router>
     <div>
-      <SignIn>
+      <Switch>
         <Route exact path={ROUTES.SIGN_IN} component={SignIn} />
         <Route path={ROUTES.SIGN_UP} component={SignUp} />
         <Route path={ROUTES.SIGN_OUT} component={SignOut} />
-        <Route path={ROUTES.DASHBOARD} component={Dashboard} />
-      </SignIn>
+        <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+      </Switch>
     </div>
   </Router>
 );
