558e48ce1c7c42268990549b2a28b67e9498d2f3
Sherri Booher <sherribooher74@gmail.com>
2019-10-26 21:40:56 -0400

    One more time to fix this App.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Dashboard from "./components/Dashboard";
 import SignIn from "./components/SignIn/SignIn";
