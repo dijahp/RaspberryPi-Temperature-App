b24fa4cea50f28d6b36aebf120faea8e7a15ff1e
Sherri Booher <sherribooher74@gmail.com>
2019-10-31 16:16:42 -0400

    Fix route so Sign In is loaded first

--- src/components/SignOut/signout.js
+++ src/components/SignOut/signout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
