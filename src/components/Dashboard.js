15896051c3383e5d7565d10a20670d943a0523a5
dijahp <dijah@bellsouth.net>
2019-11-02 11:42:32 -0400

    Updating dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import "../assets/Dashboard.css";
 import Header from "./Header";
 import Chart from "./Chart";
-import Form from "./Form";
+
 import Card from "./Card";
-import FormContainer from "./FormContainer";
+
 import Sidebar from "./Sidebar";
 import Thermo from "../assets/IMG/temperature.svg";
 import Humid from "../assets/IMG/humidity.svg";
-import Pressure from "../assets/IMG/pressure.svg";
+import Pin from '../assets/IMG/pin.svg';
 import Calendar from "../assets/IMG/calendar.svg";
 import Chart2 from "./Chart2";
 
@@ -20,31 +20,28 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
-      AvailableSensors: [{
-        name: "testsensor",
-        location: "Atlanta, GA"
-      }, 
-      {
-        name: "testsensor2",
-        location: "Dallas, Texas"
-      }
-    ]
+      AvailableSensors: [
+        {
+          name: "testsensor",
+          location: "Atlanta, GA"
+        },
+        {
+          name: "testsensor2",
+          location: "Dallas, Texas"
+        }
+      ]
     };
     this.handleInputChange = this.handleInputChange.bind(this);
-
   }
 
   handleInputChange(e) {
-
-
     this.setState({
-     AvailableSensors: [...this.state.AvailableSensors, {[e.target.name]: e.target.value}]
+      AvailableSensors: [
+        ...this.state.AvailableSensors,
+        { [e.target.name]: e.target.value }
+      ]
     });
-
   }
-
-
 
   render() {
     return (
@@ -62,21 +59,12 @@ class Dashboard extends Component {
               cardData='30ºF'
             />
             <Card iconImg={Humid} cardDesc='Humidity' cardData='43%' />
-            <Card iconImg={Pressure} cardDesc='Pressure' cardData='14.55' />
+            <Card iconImg={Pin} cardDesc='Location' cardData='Atlanta' />
             <Card iconImg={Calendar} cardDesc='Date' cardData={today} />
           </div>
           <div className='Chart-section'>
             <Chart />
-          </div>
-          <div className='Dashboard-middle'>
-            <div className='Middle-left'>
-              <Chart2 />
-            </div>
-            <div className='Middle-right'>
-              <div className='Form-container'>
-                <FormContainer availableSensors={this.state.AvailableSensors} handleInputChange={this.handleInputChange}/>
-              </div>
-            </div>
+            <Chart2 />
           </div>
         </div>
       </div>
