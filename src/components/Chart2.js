159d6e08a4ec7a411cbf229eaff6dfae6731c013
Sherri Booher <sherribooher74@gmail.com>
2019-11-02 14:18:34 -0400

    Add bar chart

--- src/components/Chart2.js
+++ src/components/Chart2.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import sensorBarChart from "./Charts";
 import "../assets/Chart2.css";
 
 class Chart2 extends Component {
   render() {
-    return <div className='Chart2'>Empty Chart</div>;
+    return (
+      <div className="Chart2">
+        <sensorBarChart />
+      </div>
+    );
   }
 }
 
