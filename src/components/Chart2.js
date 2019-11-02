ef5b36312dffb59547f0fd7c21653ddae67e548b
Samhita Noone <samhita.noone@gmail.com>
2019-11-02 23:47:09 +0000

    added bar chart showing average temperatures

--- src/components/Chart2.js
+++ src/components/Chart2.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "../assets/Chart2.css";
+import BarChartComp from "./Charts/barchart.js";
 
 class Chart2 extends Component {
   render() {
-    return <div className='Chart2'>Empty Chart</div>;
+    return <div className='Chart2'>
+          <BarChartComp />
+    </div>;
   }
 }
 
