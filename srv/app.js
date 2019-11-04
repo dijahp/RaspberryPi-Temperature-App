b7c4badaa013d52ace636bdb8ab71093ffd03064
dijahp <dijah@bellsouth.net>
2019-11-04 07:38:25 -0500

    added express build command

--- srv/app.js
+++ srv/app.js
@@ -36,6 +36,8 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'build')))
+
 app.use((req, res, next) => {
   if (!req.header("X-Auth-Token")) {
     return res.status(403).json({ error: "No auth token sent!" });
