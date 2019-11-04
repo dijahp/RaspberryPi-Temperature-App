8d2b06b531f960b011f1c70b647a224e6dc079ab
Samhita Noone <samhita.noone@gmail.com>
2019-11-04 20:00:36 +0000

    dropdown has friendlyname now

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,44 +4,46 @@ import "../assets/Sidebar.css";
 
 import { withFirebase } from './Firebase';
 var sensors = [];
+var sensorMap = [];
 
 class Sidebar extends Component {
   constructor(props) {
     super(props);
-    this.state = {childVal:''};
+    this.state = {childVal:'',
+                  friendlyName:''};
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.props.handleSensor(event.target.value);
-    this.setState({childVal: event.target.value});
+    var key = sensorMap.find(o => o.friendlyName === event.target.value);
+    this.props.handleSensor(key["sensorKey"]);
+    this.setState({childVal: key["sensorKey"], friendlyName: key['friendlyName']});
   }
 
 
   render() {
-    let initialSensor = this.props.selectedSensor;
-    let sensorRef = this.props.firebase.fs.collection('sensorData').where("humidity", ">", 0).onSnapshot((querySnapshot) => {
-
-      querySnapshot.forEach((doc) => {
-        var newItem = doc.data().sensorKey;
-        if (sensors.indexOf(newItem) === -1) {
-          sensors.push(doc.data().sensorKey)
-        }
-      }) })
+ 
+      let sensorRef = this.props.firebase.fs.collection('sensorMeta').onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          var newItem = doc.data().friendlyName;
+          var sensorKey = doc.data().sensorKey;
+          if (sensors.indexOf(newItem) === -1) {
+            sensors.push(doc.data().friendlyName);
+            sensorMap.push({"friendlyName": newItem, "sensorKey": sensorKey});
+          }
+        }) })  
     
-    var message='You selected '+initialSensor;
     return (
       <div className='Sidebar-section' >
         <h1>Fire and Ice</h1>
         <div id="Dropdownmenu">
           <h4>Sensors:</h4>
-          <select value={this.state.childVal} onChange={this.handleChange} >
+          <select value={this.state.friendlyName} onChange={this.handleChange} >
             {sensors.map((sensor, index) =>
               <option key={index} value={sensor}>{sensor}</option>
             )}
 
           </select>
-          <p>{message}</p>
         </div>
       </div>
     );
